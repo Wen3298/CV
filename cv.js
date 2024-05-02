7c11106fb92244b5faefe632c00f21fa7233041a
wen3298 <wzelayavega@gmail.com>
2024-05-02 10:26:30 -0600

    ultima parte del CV

--- cv.js
+++ cv.js
@@ -1,2 +1,21 @@
-// imagen//
+let hobbyUno = document.getElementById("primerHobby")
 
+hobbyUno.addEventListener("click",()=>{
+    alert("Me gusta escuchar musica de los 70's y 80's")
+})
+
+let hobbyDos = document.getElementById("segundoHobby")
+
+hobbyDos.addEventListener("click",()=>{
+    alert("Me gusta pintar cuando estoy estresada")
+})
+let hobbyTres = document.getElementById("tercerHobby")
+
+hobbyTres.addEventListener("click",()=>{
+    alert("Me gusta jugar Crash Bandicoot y Cuphead")
+})
+let hobbyCuatro = document.getElementById("cuartoHobby")
+
+hobbyCuatro.addEventListener("click",()=>{
+    alert("Me gusta salir a caminar en la noche ")
+})
